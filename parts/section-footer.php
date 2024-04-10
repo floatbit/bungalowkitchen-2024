ba9904039bcb547f460203a15c4d2d03e022c91e
“Chris <chris@floatbit.com>
2024-04-10 09:46:41 -0400

    blocks

--- parts/section-footer.php
+++ parts/section-footer.php
@@ -1,3 +1,114 @@
 <footer>
-  <h2>TODO: section-footer.php</h2>
+  <div class="container">
+    <div class="flex gap-10">
+      <div class="basis-4/12">
+        <h3>Follow Us</h3>
+        <p class="social">
+        <a href="#">
+            <img src="<?php echo assets_url('/dist/images/icon-facebook.png'); ?>"/>
+          </a>
+          <a href="#">
+            <img src="<?php echo assets_url('/dist/images/icon-instagram.png'); ?>"/>
+          </a>
+        </p>
+        <h3>Call Us</h3>
+        <p>
+          <a href="tel:415-366-4088">415-366-4088</a>
+        </p>
+        <h3>Visit Us</h3>
+        <p>5 Main Street Tiburon <br>California 94920</p>
+      </div>
+      <div class="basis-4/12">
+        <div class="newsletter-signup">
+          <h3>Keep Up With<br>The Good Times</h3>
+          <form action="#" class="pre-newsletter">
+            <div class="inputs">
+              <input type="text" placeholder="Type your email here"><input type="submit" value="YEAH!">
+            </div>
+          </form>
+          <div class="reveal">
+
+            <div class="reveal-content">
+              <button class="close-button" data-close aria-label="Close modal" type="button">
+                <span aria-hidden="true">&times;</span>
+              </button>
+
+                <!-- Begin Mailchimp Signup Form -->
+                <link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
+                <style type="text/css">
+                  #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+                  /* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
+                    We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+                </style>
+                <div id="mc_embed_signup">
+                <form action="https://wednesdayseven.us4.list-manage.com/subscribe/post?u=c78bdd4d07c452911c36d1e0a&amp;id=df329956e6" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                    <div id="mc_embed_signup_scroll">
+                  <h2>Subscribe</h2>
+                <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+                <div class="mc-field-group">
+                  <label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
+                </label>
+                  <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+                </div>
+                <div class="mc-field-group">
+                  <label for="mce-FNAME">First Name  <span class="asterisk">*</span>
+                </label>
+                  <input type="text" value="" name="FNAME" class="required" id="mce-FNAME">
+                </div>
+                <div class="mc-field-group">
+                  <label for="mce-LNAME">Last Name  <span class="asterisk">*</span>
+                </label>
+                  <input type="text" value="" name="LNAME" class="required" id="mce-LNAME">
+                </div>
+                <div class="mc-field-group">
+                  <label for="mce-MMERGE5">Zip Code  <span class="asterisk">*</span>
+                </label>
+                  <input type="text" value="" name="MMERGE5" class="required" id="mce-MMERGE5">
+                </div>
+                <div class="mc-field-group size1of2">
+                  <label for="mce-MMERGE6-month">Birthday </label>
+                  <div class="datefield">
+                    <span class="subfield monthfield"><input class="birthday " type="text" pattern="[0-9]*" value="" placeholder="MM" size="2" maxlength="2" name="MMERGE6[month]" id="mce-MMERGE6-month"></span> /
+                    <span class="subfield dayfield"><input class="birthday " type="text" pattern="[0-9]*" value="" placeholder="DD" size="2" maxlength="2" name="MMERGE6[day]" id="mce-MMERGE6-day"></span>
+                    <span class="small-meta nowrap">( mm / dd )</span>
+                  </div>
+                </div><div class="mc-field-group input-group checkboxes-location">
+                    <strong>Select a Location </strong>
+                    <ul><li><input type="checkbox" value="1" name="group[62714][1]" id="mce-group[62714]-62714-0"> <label for="mce-group[62714]-62714-0">Belmont Shore</label></li>
+                <li><input type="checkbox" value="2" name="group[62714][2]" id="mce-group[62714]-62714-1" checked> <label for="mce-group[62714]-62714-1">Tiburon</label></li>
+                </ul>
+                </div>
+                  <div id="mce-responses" class="clear">
+                    <div class="response" id="mce-error-response" style="display:none"></div>
+                    <div class="response" id="mce-success-response" style="display:none"></div>
+                  </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_c78bdd4d07c452911c36d1e0a_df329956e6" tabindex="-1" value=""></div>
+                    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+                    </div>
+                </form>
+                </div>
+                <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[5]='MMERGE5';ftypes[5]='zip';fnames[3]='MMERGE3';ftypes[3]='address';fnames[4]='MMERGE4';ftypes[4]='phone';fnames[6]='MMERGE6';ftypes[6]='birthday';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+                <!--End mc_embed_signup-->
+            </div>
+          </div>
+
+        </div>
+        <h3>Hours</h3>
+        <p>
+          <strong>BRUNCH</strong>: Sat & Sun 11am-3pm<br>
+          <strong>DINNER</strong>: Wed-Sat 5pm-9pm<br>
+          <strong>SUNSET SIPS</strong>: Wed-Sun 4pm-6pm<br>
+          <strong>LATE NIGHT</strong>: Fri & Sat 9pm-12am
+        </p>
+      </div>
+      <div class="basis-4/12 self-end">
+        <p class="bird-container text-centxer">
+          <img src="<?php echo assets_url('/dist/images/bird.png'); ?>" class="bird" />
+        </p>
+        <p class="text-right">
+          <a href="#" class="button">Make a Reservation</a>
+        </p>
+      </div>
+    </div>
+  </div>
 </footer>
