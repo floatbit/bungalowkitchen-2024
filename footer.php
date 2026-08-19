c18c0da7f80a929710db59a7bf9cc82dccf21ac9
“Chris <chris@floatbit.com>
2026-08-19 13:38:17 -0400

    removed button

--- footer.php
+++ footer.php
@@ -5,22 +5,7 @@
     <?php //endif; ?>
     
     <?php wp_footer(); ?>
-    <script>
-      window.addEventListener('load', function () {
-        if (
-          window.SevenroomsWidget &&
-          document.getElementById('sr-res-main-button')
-        ) {
-          SevenroomsWidget.init({
-            venueId: 'bungalowkitchen',
-            triggerId: 'sr-res-main-button',
-            type: 'reservations',
-            styleButton: false,
-            clientToken: ''
-          });
-        }
-      });
-    </script>
+    
 </body>
 
 </html>
